4928efbcc74e6b5b14da7176b20a0e9a3671ddad
Connor-Francis <44866501+Connor-Francis@users.noreply.github.com>
2018-11-09 15:48:01 +0400

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    private float xMovement;
+    private float yMovement;
+    
+    public float moveSpeed;
+    
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    
+    
+    void FixedUpdate()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+        yMovement = Input.GetAxis("Vertical");
+        
+        rb.velocity = new Vector2 (xMovement * moveSpeed, yMovement * moveSpeed);
     }
 }
