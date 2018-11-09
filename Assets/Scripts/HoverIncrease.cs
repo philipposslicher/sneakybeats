227ede2d73e7665771eb966c42b3d2348c461d91
ALPINE\Philippos Slicher <philipposslicher@gmail.com>
2018-11-09 21:04:50 +0400

    Menu react work

---
+++ Assets/Scripts/HoverIncrease.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverIncrease : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public float hoverIncrease;
+    private RectTransform rt;
+
+    private void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        rt.localScale *= hoverIncrease;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        rt.localScale /= hoverIncrease;
+    }
+}
---
+++ Assets/Scripts/MusicBounce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBounce : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    private RectTransform rt;
+    private float volumeScale;
+
+    private void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        float[] data = new float[16];
+        float a = 0;
+        audioSource.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        volumeScale = a / 16;
+
+        Vector2 volumeVector = new Vector2(volumeScale + 0.5f, volumeScale + 0.5f);
+        rt.localScale = Vector2.Lerp(rt.localScale, volumeVector, 0.05f);
+    }
+
+}
