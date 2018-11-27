e0eedc5a4d5177a684b2835de91a76c0542ced84
Connor McGrath <otmshank3@hotmail.com>
2018-11-27 15:17:00 +0400

    Collectables

---
+++ Assets/Scripts/GameManage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManage : MonoBehaviour {
+
+    public TextMeshProUGUI scoreBoard;
+    public int score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+    public void AddScore (int amount) {
+
+        score += amount;
+        scoreBoard.text = "Score: " + score;
+
+    } 
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+    
+    public int value;
+
+    private GameManage gameManage;
+
+	// Use this for initialization
+	void Start () {
+
+        gameManage = FindObjectOfType<GameManage> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnTriggerEnter2D (Collider2D other) {
+
+        if (other.CompareTag("Player")) {
+
+            gameManage.AddScore(value);
+            Destroy(gameObject);
+
+        }
+
+    }
+}
